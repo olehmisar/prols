22f5d911bf9729270d19450281795ce4b3eed2bd
oleh <olehmisar@gmail.com>
2024-10-05 20:05:27 +0200

    remove next.js

--- apps/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TokenSwap from "@/components/token-swap";
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      <TokenSwap />
-    </div>
-  );
-}
