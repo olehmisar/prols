242106e7a46ea653523566d78f26de78c2ca75d5
oleh <olehmisar@gmail.com>
2024-10-05 14:39:34 +0200

    more utils

--- packages/contracts/src/utils.ts
+++ packages/contracts/src/utils.ts
@@ -1,5 +1,6 @@
-import type { Currency, CurrencyAmount } from "@uniswap/sdk-core";
+import { CurrencyAmount, type Currency } from "@uniswap/sdk-core";
 import { Decimal } from "decimal.js";
+import { ethers } from "ethers";
 
 export function currencyAmountToDecimal(amount: CurrencyAmount<Currency>) {
   return new Decimal(amount.quotient.toString()).div(
@@ -7,9 +8,36 @@ export function currencyAmountToDecimal(amount: CurrencyAmount<Currency>) {
   );
 }
 
-export function toBinanceAmount(amount: CurrencyAmount<Currency>) {
+export function currencyAmountToBigInt(amount: CurrencyAmount<Currency>) {
+  return BigInt(amount.quotient.toString());
+}
+
+export function currencyAmountToBinanceAmount(
+  amount: CurrencyAmount<Currency>,
+) {
   return {
     symbol: amount.currency.symbol!,
     amount: currencyAmountToDecimal(amount),
   };
+}
+
+/**
+ * Converts user input to a CurrencyAmount. Takes into account the token's decimals.
+ * E.g., for a token with 9 decimals, `"1.23"` becomes `CurrencyAmount(1230000000)`.
+ *
+ */
+export function parseCurrencyAmount<T extends Currency>(
+  token: T,
+  userAmount: string | number,
+): CurrencyAmount<T> {
+  let rawAmount: bigint;
+  try {
+    rawAmount = ethers.parseUnits(userAmount.toString(), token.decimals);
+  } catch (e) {
+    throw new Error(`${userAmount} is too small or too large.`);
+  }
+  if (rawAmount === 0n && userAmount !== "0") {
+    throw new Error(`${userAmount} is too small`);
+  }
+  return CurrencyAmount.fromRawAmount(token, rawAmount.toString());
 }
