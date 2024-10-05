3f92a0bcc008730253343fe756e1f147f0b1aeb4
oleh <olehmisar@gmail.com>
2024-10-05 10:53:34 +0200

    feat: prols

--- packages/contracts/src/contracts/prols_router/target/ProlsRouter.ts
+++ packages/contracts/src/contracts/prols_router/target/ProlsRouter.ts
@@ -104,8 +104,49 @@ export class ProlsRouterContract extends ContractBase {
   }
   
 
-  
+  public static get storage(): ContractStorageLayout<'admin' | 'minters' | 'balances' | 'total_supply' | 'pending_shields' | 'public_balances' | 'symbol' | 'name' | 'decimals'> {
+      return {
+        admin: {
+      slot: new Fr(1n),
+    },
+minters: {
+      slot: new Fr(2n),
+    },
+balances: {
+      slot: new Fr(3n),
+    },
+total_supply: {
+      slot: new Fr(4n),
+    },
+pending_shields: {
+      slot: new Fr(5n),
+    },
+public_balances: {
+      slot: new Fr(6n),
+    },
+symbol: {
+      slot: new Fr(7n),
+    },
+name: {
+      slot: new Fr(8n),
+    },
+decimals: {
+      slot: new Fr(9n),
+    }
+      } as ContractStorageLayout<'admin' | 'minters' | 'balances' | 'total_supply' | 'pending_shields' | 'public_balances' | 'symbol' | 'name' | 'decimals'>;
+    }
+    
 
+  public static get notes(): ContractNotes<'TransparentNote' | 'TokenNote'> {
+    return {
+      TransparentNote: {
+          id: new NoteSelector(3193649735),
+        },
+TokenNote: {
+          id: new NoteSelector(2350566847),
+        }
+    } as ContractNotes<'TransparentNote' | 'TokenNote'>;
+  }
   
 
   /** Type-safe wrappers for the public methods exposed by the contract. */
@@ -113,6 +154,12 @@ export class ProlsRouterContract extends ContractBase {
     
     /** compute_note_hash_and_optionally_a_nullifier(contract_address: struct, nonce: field, storage_slot: field, note_type_id: field, compute_nullifier: boolean, serialized_note: array) */
     compute_note_hash_and_optionally_a_nullifier: ((contract_address: AztecAddressLike, nonce: FieldLike, storage_slot: FieldLike, note_type_id: FieldLike, compute_nullifier: boolean, serialized_note: FieldLike[]) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** settle(sell_token: struct, buy_token: struct, sell_amount: field, buy_amount: field, secret_hash: field, nonce: field) */
+    settle: ((sell_token: AztecAddressLike, buy_token: AztecAddressLike, sell_amount: FieldLike, buy_amount: FieldLike, secret_hash: FieldLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** withdraw(token: struct, amount: field, to: struct) */
+    withdraw: ((token: AztecAddressLike, amount: FieldLike, to: AztecAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
   };
 
   
